7383579f07b777772399b824a788484bc1c24c8f
kadoy <ekostenkodev@gmail.com>
2021-03-11 23:47:24 +0700

    add camera movement, fix color tweener

---
+++ Assets/Client/Scripts/Scenes/CrimeNet/Camera/CameraMovement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace Kadoy.CrimeNet.MenuCamera {
+  public class CameraMovement : MonoBehaviour {
+    [SerializeField]
+    private Camera movableCamera;
+
+    [Space]
+    [SerializeField]
+    private Collider2D boundsRoot;
+
+    [Header("SETTINGS")]
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Vector2 thickness;
+
+    private Transform root;
+    private Vector2 bounds;
+
+    private void Awake() {
+      var screenSize = new Vector3(movableCamera.pixelWidth, movableCamera.pixelHeight);
+      var worldScreenSize = movableCamera.ScreenToWorldPoint(screenSize);
+      
+      root = movableCamera.transform;
+      bounds = boundsRoot.bounds.size * 0.5f - worldScreenSize;
+    }
+
+    private void Update() {
+      var cameraPosition = root.position;
+      var mousePosition = Input.mousePosition;
+      var moveDirection = Vector3.zero;
+
+      var isOverTop = mousePosition.y >= Screen.height - thickness.y;
+      var isOverBottom = mousePosition.y <= thickness.y;
+      var isOverRight = mousePosition.x >= Screen.width - thickness.x;
+      var isOverLeft = mousePosition.x <= thickness.x;
+
+      moveDirection.y = isOverTop ? 1 : isOverBottom ? -1 : 0;
+      moveDirection.x = isOverRight ? 1 : isOverLeft ? -1 : 0;
+
+      moveDirection *= speed * Time.deltaTime;
+      cameraPosition += moveDirection;
+
+      cameraPosition.x = Mathf.Clamp(cameraPosition.x, -bounds.x, bounds.x);
+      cameraPosition.y = Mathf.Clamp(cameraPosition.y, -bounds.y, bounds.y);
+
+      root.position = cameraPosition;
+    }
+  }
+}
--- Assets/Client/Scripts/Utils/DotTweenExtensions/ColorTweenerBehaviour.cs
+++ Assets/Client/Scripts/Utils/DotTweenExtensions/ColorTweenerBehaviour.cs
@@ -4,34 +4,25 @@ using UnityEngine.UI;
 
 namespace Kadoy.CrimeNet.Utils {
   public class ColorTweenerBehaviour : TweenerBehaviour<Color> {
-    private SpriteRenderer spriteRenderer;
-    private Image image;
-    private RawImage rawImage;
+    private Graphic graphic;
+    private Renderer renderer;
 
     private void Awake() {
-      spriteRenderer = GetComponent<SpriteRenderer>();
-      image = GetComponent<Image>();
-      rawImage = GetComponent<RawImage>();
+      graphic = GetComponent<Graphic>();
+      renderer = GetComponent<Renderer>();
     }
 
     public override void Animate() {
-      if (spriteRenderer != null) {
-        spriteRenderer.color = startValue;
-        tweener = spriteRenderer
+      if (graphic != null) {
+        graphic.color = startValue;
+        tweener = graphic
           .DOColor(endValue, duration)
           .SetEase(animationCurve)
           .SetLoops(isLoop ? int.MaxValue : 0, loopType)
           .SetDelay(delay);
-      } else if (image != null) {
-        image.color = startValue;
-        tweener = image
-          .DOColor(endValue, duration)
-          .SetEase(animationCurve)
-          .SetLoops(isLoop ? int.MaxValue : 0, loopType)
-          .SetDelay(delay);
-      }else if (rawImage != null) {
-        rawImage.color = startValue;
-        tweener = rawImage
+      } else if (renderer != null) {
+        renderer.material.color = startValue;
+        tweener = renderer.material
           .DOColor(endValue, duration)
           .SetEase(animationCurve)
           .SetLoops(isLoop ? int.MaxValue : 0, loopType)
@@ -40,14 +31,12 @@ namespace Kadoy.CrimeNet.Utils {
     }
 
     public override void StopAnimation() {
-      if (spriteRenderer != null) {
-        spriteRenderer.color = startValue;
-      } else if (image != null) {
-        image.color = startValue;
-      }else if (rawImage != null) {
-        rawImage.color = startValue;
+      if (graphic != null) {
+        graphic.color = startValue;
+      } else if (renderer != null) {
+        renderer.material.color = startValue;
       }
-      
+
       base.StopAnimation();
     }
   }
